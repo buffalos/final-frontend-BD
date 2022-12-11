5792ece4685534391da945e30a49634618bd6d02
bahtija <bahtija.durakovic61@myhunter.cuny.edu>
2022-12-11 01:01:55 -0500

    edit container props and form

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import {
   EditCourseContainer
 } from './components/containers';
 
-// if you create separate components for adding/editing 
+// if you create separate components for adding/editing
 // a student or instructor, make sure you add routes to those
 // components here
 
@@ -24,15 +24,18 @@ const App = () => {
         <Route exact path="/" component={HomePageContainer} />
         <Route exact path="/instructors" component={AllInstructorsContainer} />
         <Route exact path="/instructor/:id" component={InstructorContainer} />
+
+        <Route exact path="/editinstructor/:id" component={EditInstructorContainer} />
+        <Route exact path="/newinstructor" component={NewInstructorContainer} />
+
         <Route exact path="/courses" component={AllCoursesContainer} />
         <Route exact path="/newcourse" component={NewCourseContainer} />
         <Route exact path="/course/:id" component={CourseContainer} />
         <Route exact path="/editcourse/:id" component={EditCourseContainer} />
 
-      </Switch>        
+      </Switch>
     </div>
   );
 }
 
 export default App;
-
