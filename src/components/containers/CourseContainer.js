dc4529f6f73c2e7c726f7eb163763f3979a855bb
bahtija <bahtija.durakovic61@myhunter.cuny.edu>
2022-12-11 14:40:03 -0500

    delete course attempt

--- src/components/containers/CourseContainer.js
+++ src/components/containers/CourseContainer.js
@@ -1,18 +1,51 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { fetchCourseThunk } from "../../store/thunks";
+import { fetchCourseThunk, deleteCourseThunk } from "../../store/thunks";
+import { Redirect } from 'react-router-dom';
 import { CourseView } from "../views";
 
 class CourseContainer extends Component {
-  componentDidMount() {
+    constructor(props) {
+      super(props);
+      this.state = {
+        r: false,
+      }
+    }
+    componentDidMount() {
     //getting course ID from url
-    this.props.fetchCourse(this.props.match.params.id);
+        this.props.fetchCourse(this.props.match.params.id);
+        console.log("this.props = " + this.props);
+        console.log("this.props.match.params.id = " + this.props.match.params.id);
+    }
+
+    handleClick = event => {
+      //event.preventDefault();
+      //get new info for course from form input
+      let courseid = {
+          id: this.props.course.id,
+      };
+
+      this.props.deleteCourse(courseid);
+      //this.props.r = true;
+
+      this.setState({
+          r: true
+      });
+      console.log("state: " + this.state.r);
+  }
+
+  componentWillUnmount() {
+      this.setState({r: false});
   }
 
   render() {
+      if(this.state.r){
+          return(<Redirect to={`/courses`}/>)
+      }
     return (
       <CourseView
         course={this.props.course}
+        handleClick={this.handleClick}
       />
     );
   }
@@ -29,6 +62,7 @@ const mapState = (state) => {
 const mapDispatch = (dispatch) => {
   return {
     fetchCourse: (id) => dispatch(fetchCourseThunk(id)),
+    deleteCourse: (courseId) => dispatch(deleteCourseThunk(courseId)),
   };
 };
 
