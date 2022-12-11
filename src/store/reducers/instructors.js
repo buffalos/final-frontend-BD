1a9e01bbfaa8a457e6b6a9d7e2057553acfd434b
bahtija <bahtija.durakovic61@myhunter.cuny.edu>
2022-12-11 00:52:21 -0500

    added instructor reducers and types

--- src/store/reducers/instructors.js
+++ src/store/reducers/instructors.js
@@ -5,8 +5,18 @@ const allInstructors = (state = [], action) => {
   switch (action.type) {
     case at.FETCH_ALL_INSTRUCTORS:
       return action.payload;
-    default:
-      return state;
+      case at.ADD_INSTRUCTOR:
+        return [...state, action.payload]
+      case at.DELETE_INSTRUCTOR:
+        return state.filter(instructor => instructor.id!==action.payload);
+      case at.EDIT_INSTRUCTOR:
+        return state.map(instructor => { 
+          return (
+            instructor.id===action.payload.id ? action.payload : instructor
+          );
+        });
+      default:
+        return state;
   }
 };
 
