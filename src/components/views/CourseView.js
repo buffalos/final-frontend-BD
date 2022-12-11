eb31fefe5f1bc85b2fdc593bf3c0978f3f1b11d5
bahtija <bahtija.durakovic61@myhunter.cuny.edu>
2022-12-11 06:22:27 -0500

    back to working version

--- src/components/views/CourseView.js
+++ src/components/views/CourseView.js
@@ -1,17 +1,14 @@
 import { Link } from "react-router-dom";
 
 const CourseView = (props) => {
-   let {course, handleClick} = props;
+  const { course } = props;
   return (
     <div>
       <h1>{course.title}</h1>
       <Link to={`/instructor/${course.instructor.id}`}> {course.instructor ? <h3>{course.instructor.firstname + " " + course.instructor.lastname}</h3>: <h3>Staff</h3>} </Link>
       <Link to={`/editcourse/${course.id}`}>Edit course information</Link>
-
       <br/>
       <Link to={`/courses`}>View all courses</Link>
-      <br/>
-       <button onClick={(e) => this.handleClick(e)}>Delete</button>
     </div>
   );
 
