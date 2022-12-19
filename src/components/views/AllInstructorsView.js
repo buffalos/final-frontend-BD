df6c930d94b87c7014dc7468a17c2cee5a7e7856
bahtija <bahtija.durakovic61@myhunter.cuny.edu>
2022-12-19 05:36:56 -0500

    minor error fixed

--- src/components/views/AllInstructorsView.js
+++ src/components/views/AllInstructorsView.js
@@ -2,7 +2,6 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import styles from '../../mystyle.module.css'; 
 //import { deleteInstructor } from "../../store/actions/actionCreators";
-import styles from '../../mystyle.module.css';
 
 const AllInstructorsView = (props) => {
   
